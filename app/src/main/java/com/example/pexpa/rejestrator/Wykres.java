bb9fcc40055ff68b67ffa023f5131a9cd9e22d5b
Dawid <dawid.k92@gmail.com>
2016-11-30 23:41:38 +0100

    BD

--- app/src/main/java/com/example/pexpa/rejestrator/Wykres.java
+++ app/src/main/java/com/example/pexpa/rejestrator/Wykres.java
@@ -3,6 +3,7 @@ package com.example.pexpa.rejestrator;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.Point;
 import android.support.v4.util.LongSparseArray;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -13,16 +14,22 @@ import android.widget.SeekBar;
 import android.widget.TextView;
 
 import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
 import com.github.mikephil.charting.utils.ColorTemplate;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.BarGraphSeries;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.DataPointInterface;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Random;
 
 import bazadanych.DBManager;
 import bazadanych.Event;
@@ -36,55 +43,59 @@ public class Wykres extends AppCompatActivity {
     BarDataSet Bardataset;
     BarData BARDATA;
     TextView textView;
+    GraphView graph;
     int [] intervalValues = {1,5,10,15,20,30};
     DBManager db = new DBManager(this);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wykres);
-        chart = (BarChart) findViewById(R.id.chart);
+         graph = (GraphView) findViewById(R.id.graph123);
+      //  chart = (BarChart) findViewById(R.id.chart);
         textView = (TextView) findViewById(R.id.textViewWykres);
         SeekBar seekBar = (SeekBar) findViewById(R.id.seekBarWykres);
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
                 textView.setText("Interwały: " +intervalValues[i]+" min.");
-                List<BarEntry> entries = new ArrayList<>();
-long patientID = getIntent().getExtras().getLong("idTerapii");
-ArrayList<Therapy> therapies = db.getAllTherapies(" id ="+getIntent().getExtras().getLong("idTerapii"));
+                ArrayList<DataPoint> entries = new ArrayList<>();
+                entries.add(new DataPoint(0,0));
+                long patientID = getIntent().getExtras().getLong("idTerapii");
+                ArrayList<Therapy> therapies = db.getAllTherapies(" id ="+getIntent().getExtras().getLong("idTerapii"));
                 long start = therapies.get(0).getStartDate().getTime();
                 long end = therapies.get(0).getEndDate().getTime();
                 long addTime = intervalValues[i]*1000;
                 SimpleDateFormat SDF =new  SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-                int k =0;
+                int k =1;
                 int sizeMax = 0;
 
                 for(long j= start;j<= end;j+=addTime){
                     String startString = SDF.format(j);
                     String endString = SDF.format(j+addTime);
                     ArrayList<Event> EventList = db.getAllEvents(" event_time>='"+startString +"' AND event_time<'"+endString+"';");
-                    if(EventList.size()>0)entries.add(new BarEntry((float) (k-0.5),EventList.size()));
+                    if(EventList.size()>0)entries.add(new DataPoint(k,EventList.size()));
                     if(sizeMax< EventList.size())
                     sizeMax = EventList.size();
                     k++;
                 }
-                chart.getXAxis().setValueFormatter(new HourFormatter(intervalValues[i]));
+                DataPoint[] points = new DataPoint [entries.size()];
+                for(int j=0; j<entries.size(); j++) points[j] = entries.get(j);
+                BarGraphSeries<DataPoint> series = new BarGraphSeries<>(points);
 
-sizeMax*=1.5;
-                BarDataSet set = new BarDataSet(entries, "BarDataSet");
-                BarData data = new BarData(set);
-                //data.setBarWidth(0.9f); // set custom bar width
-                chart.getAxisLeft().setAxisMinimum(0);
-                chart.getAxisLeft().setAxisMaximum(sizeMax);
-                chart.getAxisRight().setAxisMinimum(0);
-                chart.getAxisRight().setAxisMaximum(sizeMax);
-                chart.setData(data);
+                graph.removeAllSeries();
+               graph.addSeries(series);
 
+                graph.getViewport().setMinX(0);
+               graph.getViewport().setMaxX(10);
+                graph.getViewport().setMaxY(sizeMax*1.5);
+                graph.getViewport().setMinY(0);
 
-                //chart.setFitBars(true); // make the x-axis fit exactly all bars
-                chart.invalidate(); // refresh
-//chart.getXAxis().setGranularityEnabled(true);
-               // chart.getXAxis().setGranularity(intervalValues[i]);
+               series.setSpacing(15);
+                graph.getViewport().setXAxisBoundsManual(true);
+                graph.getViewport().setYAxisBoundsManual(true);
+               graph.getViewport().setScrollable(true);
+
             }
 
             @Override
@@ -110,22 +121,28 @@ sizeMax*=1.5;
         });
 
 
-        List<BarEntry> entries = new ArrayList<>();
-        entries.add(new BarEntry(0f, 30f));
-        entries.add(new BarEntry(1f, 80f));
-        entries.add(new BarEntry(2f, 60f));
-        entries.add(new BarEntry(3f, 50f));
-        // gap of 2f
-        entries.add(new BarEntry(5f, 70f));
-        entries.add(new BarEntry(6f, 60f));
+        Random r = new Random();
 
-        BarDataSet set = new BarDataSet(entries, "BarDataSet");
 
-        BarData data = new BarData(set);
-        data.setBarWidth(0.9f); // set custom bar width
-        chart.setData(data);
-        chart.setFitBars(true); // make the x-axis fit exactly all bars
-        chart.invalidate(); // refresh
+        DataPoint[] points = new DataPoint[2];
+        points[0] = new DataPoint(0,0);
+        for (int i = 1; i < points.length; i++) {
+            points[i] = new DataPoint(i, r.nextInt(25));
+        }
+
+        //graph = (GraphView) findViewById(R.id.graph);
+        BarGraphSeries<DataPoint> series = new BarGraphSeries<>(points);
+        //series.
+        graph.addSeries(series);
+        series.setSpacing(15);
+        graph.getViewport().setMinX(0);
+       graph.getViewport().setMaxX(10);
+
+        graph.getViewport().setXAxisBoundsManual(true);
+
+        graph.getViewport().setScrollable(true); // enables horizontal scrolling
+
+
     }
 
 
